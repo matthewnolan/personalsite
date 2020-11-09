4b41a1dbd403570cf13e91a8d60b40d3337de574
Matthew Nolan <matthewnolan415@gmail.com>
2020-11-09 21:33:33 +0700

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,23 +33,30 @@ const App: React.FC = ({ Component, pageProps }: any) => {
           ></script>
         )}
 
-        <meta name="description" content="Matthew Nolan Blog. Entrepreneur, Engineer, Artist." />
+        <meta name="description" content="Hi I'm Matthew Nolan, an entrepreneur, engineer, and artist. I build tech that makes the world more open and fun." />
+
+        <meta name="keywords" content="Matthew Nolan, blockchain, app, Verona, Menlo One" />
+
         <meta name="robots" content="index, follow" />
+        <meta name="author" content="Matthew Nolan" />
         <meta name="url" content="https://matthewnolan.xyz" />
         <meta name="identifier-URL" content="https://matthewnolan.xyz" />
+
         <meta http-equiv="content-language" content="en" />
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@ImMatthewNolan" />
         <meta name="twitter:title" content="Matthew Nolan Blog" />
-        <meta name="twitter:description" content="Matthew Nolan Thoughts on Tech" />
+        <meta name="twitter:description" content="Matthew Nolan. Entrepreneur, Engineer, Artist." />
         <meta name="twitter:creator" content="@ImMatthewNolan" />
-        <meta name="twitter:url" content="http://matthewnolan.xyz" />
+        <meta name="twitter:url" content="https://matthewnolan.xyz" />
         <meta name="twitter:image" content="https://matthewnolan.xyz/images/matthew-nolan-thumbnail.jpg" />
 
         <link rel="canonical" href="https://matthewnolan.xyz/" />
 
         <link rel="icon" href="/favicon.ico" />
+
+
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
 
