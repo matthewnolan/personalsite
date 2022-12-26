1045ce4a520549f092b3b8ae28364b0f449105ab
Matthew Nolan <matthewnolan415@gmail.com>
2022-12-26 20:33:40 +0700

    fixing tw

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -33,13 +33,24 @@ export const Meta: React.FC<{
       <meta property="og:site_name" content={globals.siteName} />
       {meta.link && <meta property="og:url" content={`${globals.url+meta.link}`} />}
 
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={meta.title} />
-      {meta.desc && <meta name="twitter:description" content={meta.desc} />}
       <meta name="twitter:site" content={globals.twitterHandle} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:label1" content="Reading Time" />
+      <meta name="twitter:tile:template:testing" content="2" />
+      <meta name="twitter:tile:info1:icon" content="Person" />
+      <meta name="twitter:tile:info2:icon" content="Calendar" />
+      <meta name="twitter:cta" content="Read on matthewnolan.xyz" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:creator" content={globals.twitterHandle} />
+      <meta name="twitter:data1" content="10 min read" />
+      {meta.desc && <meta name="twitter:description" content={meta.desc} />}
       <meta name="twitter:creator" content={globals.twitterHandle} />
       {meta.image && <meta name="twitter:image" content={globals.url + meta.image} />}
+      {meta.image && <meta name="twitter:tile:image" content={globals.url + meta.image} />}
       {meta.image && <meta property="og:image" content={`${globals.url + meta.image}`} />}
+      <meta name="twitter:tile:info1:text" content="Matthew Nolan" />
+      <meta name="twitter:tile:info2:text" content="Dec 15, 2022" />
+
     </NextHead>
   );
 };
